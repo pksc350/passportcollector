56cb2f6ca1378769723ed6a5173caf19dbc3767d
Kaye Roberts <pkaye@kayes-mbp.lan>
2021-04-18 11:22:32 -0700

    About page rendering

--- main_app/views.py
+++ main_app/views.py
@@ -6,3 +6,6 @@ from django.http import HttpResponse
 
 def home(request):
     return HttpResponse('<h1>Hello!</h1>')
+
+def about(request):
+    return HttpResponse('<h1>About the Passport Stamp Collector</h1>')
