9f88577598f341c60b639b331d2dc03c62dd2fd9
Kaye Roberts <pkaye@kayes-mbp.lan>
2021-04-18 14:07:38 -0700

    Created the admin login

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Stamp (models.Model):
+    country = models.CharField(max_length=100)
+    date = models.DateField()
+    color = models.CharField(max_length=50)
+    shape = models.CharField(max_length=50)
+
+def __str__(self):
+    return self.country
--- main_app/views.py
+++ main_app/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render
 from .stamp_class import stamps
+from .models import Stamp
 
 # Create your views here.
 from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse('<h1>Hello!</h1>')
+    return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
 
 def stamps_index(request):
+    stamps = Stamp.objects.all()
     return render(request, 'stamps/index.html', {'stamps': stamps})
