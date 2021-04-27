934012f311415f250e8186d89b9aa653e1b78ef2
Kaye Roberts <pkaye@Kayes-MacBook-Pro.local>
2021-04-26 20:24:30 -0700

    Commit before deploy

--- main_app/urls.py
+++ main_app/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('stamps/create', views.StampCreate.as_view(), name='stamps_create'),
     path('stamps/<int:pk>/update', views.StampUpdate.as_view(), name='stamps_update'),
     path('stamps/<int:pk>/delete', views.StampDelete.as_view(), name='stamps_delete'),
+    path('accounts/signup/', views.signup, name='signup'),
     path('stamps/<int:stamp_id>/add_weather', views.add_weather, name='add_weather'),
     path('stamps/<int:stamp_id>/assoc_attraction/<int:attraction_id>/', views.assoc_attraction, name='assoc_attraction'),
     path('stamps/<int:stamp_id>/add_photo/', views.add_photo, name='add_photo'),
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, redirect
 from .stamp_class import stamps
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Stamp, Attraction, Photo
 from .forms import WeatherForm
 
@@ -20,11 +24,12 @@ def home(request):
 def about(request):
     return render(request, 'about.html')
 
+@login_required
 def stamps_index(request):
-    stamps = Stamp.objects.all()
+    stamps = Stamp.objects.filter(user=request.user)
     return render(request, 'stamps/index.html', {'stamps': stamps})
 
-
+@login_required
 def stamp_details(request, stamp_id):
     stamp = Stamp.objects.get(id=stamp_id)
     attractions_i_have_not_seen = Attraction.objects.exclude(id__in = stamp.attractions.all().values_list('id'))
@@ -36,6 +41,7 @@ def stamp_details(request, stamp_id):
     }
     return render(request, 'stamps/detail.html', context)
 
+@login_required
 def add_weather(request, stamp_id):
     form = WeatherForm(request.POST)
 
@@ -45,25 +51,44 @@ def add_weather(request, stamp_id):
         new_weather.save()
     return redirect ('detail', stamp_id=stamp_id)
 
+def signup(request):
+    error_message = ''
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login (request, user)
+            return redirect('index')
+        else:
+            error_message = 'Invalid sign up - try again'
+    form = UserCreationForm()
+    context = {'form': form, 'error_message': error_message}
+    return render(request, 'registration/signup.html', context)
 
-class StampCreate(CreateView):
+
+class StampCreate(LoginRequiredMixin, CreateView):
     model = Stamp
-    fields = '__all__'
+    fields = ['country', 'date', 'color', 'shape']
+    success_url = '/stamps/'
 
-class StampUpdate(UpdateView):
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+class StampUpdate(LoginRequiredMixin, UpdateView):
     model = Stamp
     fields = ['date', 'color', 'shape']
 
-class StampDelete(DeleteView):
+class StampDelete(LoginRequiredMixin, DeleteView):
     model = Stamp
     success_url = '/stamps/'
 
-
+@login_required
 def assoc_attraction(request, stamp_id, attraction_id):
     Stamp.objects.get(id=stamp_id).attractions.add(attraction_id)
     return redirect('detail', stamp_id=stamp_id)
 
-
+@login_required
 def add_photo(request, stamp_id):
     photo_file = request.FILES.get('photo-file', None)
     if photo_file:
